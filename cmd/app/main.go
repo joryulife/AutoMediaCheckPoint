394a5383e3bfe209d8e9a6e1de17812dd938616b
joryulife <joryulife@gmail.com>
2021-09-25 04:11:56 +0900

    プレ完成

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -15,10 +17,15 @@ import (
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/atotto/clipboard"
+	"github.com/joryulife/AutoMediaCheckPoint/pkg/GCP"
+	"github.com/joryulife/AutoMediaCheckPoint/pkg/movie"
+	"github.com/joryulife/AutoMediaCheckPoint/pkg/sound"
 	StringTime "github.com/joryulife/AutoMediaCheckPoint/pkg/time"
+	"github.com/joryulife/AutoMediaCheckPoint/pkg/word"
 )
 
 func main() {
+
 	if f, err := os.Stat("../../lib/time"); os.IsNotExist(err) || !f.IsDir() {
 		if err := os.Mkdir("../../lib/time", 0777); err != nil {
 			log.Println(err)
@@ -46,8 +53,10 @@ func main() {
 			loginstatus := true
 			if loginstatus {
 				loginMessage.Text = "now you are logged : " + inputID.Text
+				loginMessage.Refresh()
 			} else {
 				loginMessage.Text = "ID or password id is incorrect"
+				loginMessage.Refresh()
 			}
 		},
 		),
@@ -63,18 +72,21 @@ func main() {
 	fileNameSet = false
 	recordFileMessage := widget.NewLabel("Enter filename")
 	inputTimeFileName := widget.NewEntry()
-	inputTimeFileName.SetPlaceHolder("Enter FileName")
+	inputTimeFileName.SetPlaceHolder("Enter FileName : Don't need .txt")
 
 	setFileName := widget.NewButton("SET", func() {
 		if inputTimeFileName.Text == "" {
 			recordFileMessage.Text = "Please enter file name"
+			recordFileMessage.Refresh()
 			fileNameSet = false
 		} else {
 			fileNameSet = true
-			if f, err := os.Stat("../../lib/time/" + inputTimeFileName.Text); os.IsNotExist(err) || f.IsDir() {
-				recordFileMessage.Text = "create and record at :" + inputTimeFileName.Text
+			if f, err := os.Stat("../../lib/time/" + inputTimeFileName.Text + ".txt"); os.IsNotExist(err) || f.IsDir() {
+				recordFileMessage.Text = "create and record at :" + inputTimeFileName.Text + ".txt"
+				recordFileMessage.Refresh()
 			} else {
-				recordFileMessage.Text = inputTimeFileName.Text + " is already exists. \"start\" to overwrite it."
+				recordFileMessage.Text = inputTimeFileName.Text + ".txt" + " is already exists. \"start\" to overwrite it."
+				recordFileMessage.Refresh()
 			}
 		}
 	})
@@ -127,9 +139,16 @@ func main() {
 			} else {
 				log.Println("STOP")
 				recordStatus = false
+
 				t1 := time.Since(nt).Seconds()
 				str := StringTime.TimeToString(t1)
 				checkTimeList = append(checkTimeList, str)
+				checkTimeListObject.Refresh()
+			}
+		}),
+		widget.NewButton("RESET", func() {
+			if recordStatus == false {
+				checkTimeList = nil
 				checkTimeListObject.Refresh()
 			}
 		}),
@@ -160,37 +179,52 @@ func main() {
 	//##############################出力処理##############################
 
 	setSourceStatus := 0
-	inputFileSource := widget.NewEntry()
-	inputFileSource.SetPlaceHolder("Enter File Source")
-	sourceSelectRadio := widget.NewRadioGroup([]string{"by local", "by URL"}, func(value string) {
-		inputFileSource.SetPlaceHolder(value)
-		if value == "by local" {
-			setSourceStatus = 1
-		} else {
-			setSourceStatus = 2
-		}
+	setSourceMessage := widget.NewLabel("No Video Sources")
+	allsorcefile := dirwalk("../../lib/movie")
+	inputFileLocalSource := widget.NewSelect(allsorcefile, func(value string) {
+		setSourceStatus = 1
+		setSourceMessage.Text = "Select Local : " + value
+		setSourceMessage.Refresh()
 	})
-	setSourceMessage := widget.NewLabel("No Sources")
-	sourceFileCheckButton := widget.NewButton("Check Source", func() {
+	inputFileLocalSource.PlaceHolder = "By Local"
+	inputFileSource := widget.NewEntry()
+	inputFileSource.SetPlaceHolder("Enter Source URL")
+	inputFileSource.OnChanged = func(value string) {
+		setSourceStatus = 2
+		setSourceMessage.Text = "Select URL : " + value
+		setSourceMessage.Refresh()
+	}
+
+	sourceFileCheckButton := widget.NewButton("Check", func() {
 		if setSourceStatus == 0 {
 			setSourceMessage.Text = "Please select Source"
+			setSourceMessage.Refresh()
 		} else if setSourceStatus == 1 {
-			setSourceMessage.Text = inputFileSource.Text + " OK"
+			setSourceMessage.Text = inputFileLocalSource.Selected + " OK"
+			setSourceMessage.Refresh()
 		} else {
-			setSourceMessage.Text = inputFileSource.Text + " OK"
+			str := inputFileSource.Text + " OK"
+			setSourceMessage.Text = str
+			setSourceMessage.Refresh()
 		}
 	})
-	sourceFileContent := container.New(layout.NewBorderLayout(nil, nil, nil, sourceFileCheckButton), inputFileSource, sourceFileCheckButton)
-	inputTimeSource := widget.NewEntry()
-	inputTimeSource.SetPlaceHolder("Enter time Source file name")
+	sourceFiles := container.NewVBox(inputFileLocalSource, inputFileSource)
+	sourceFileContent := container.New(layout.NewBorderLayout(nil, nil, nil, sourceFileCheckButton), sourceFiles, sourceFileCheckButton)
+	alltimefile := dirwalk("../../lib/time")
+	inputTimeSource := widget.NewSelect(alltimefile, func(value string) {
+		log.Println(value)
+	})
 	setTimeSourceMessage := widget.NewLabel("No Sources")
-	timeSourceCheckButton := widget.NewButton("Check time source", func() {
-		fp, err := os.Open("../../lib/testfile/" + inputTimeSource.Text)
+	var CheckPoint []float64
+	setTimeSourceStatus := false
+	timeSourceCheckButton := widget.NewButton("Check", func() {
+		fp, err := os.Open("../../lib/time/" + inputTimeSource.Selected)
 		if err != nil {
-			setTimeSourceMessage.Text = "File does not exist : " + inputTimeSource.Text
+			setTimeSourceMessage.Text = "File does not exist : " + inputTimeSource.Selected
+			setTimeSourceMessage.Refresh()
+			setTimeSourceStatus = false
 			fp.Close()
 		} else {
-			var CheckPoint []float64
 			timeString := ""
 			scanner := bufio.NewScanner(fp)
 			for scanner.Scan() {
@@ -198,25 +232,62 @@ func main() {
 			}
 			CheckPoint = StringTime.StringToTime(timeString)
 			fmt.Println(CheckPoint)
-			apath, _ := filepath.Abs("../../lib/time/" + inputTimeSource.Text)
+			apath, _ := filepath.Abs("../../lib/time/" + inputTimeSource.Selected)
 			setTimeSourceMessage.Text = "Set file : " + apath
+			setTimeSourceMessage.Refresh()
+			log.Println(apath)
 			fp.Close()
+			setTimeSourceStatus = true
 		}
 	})
 	timeSourceContent := container.New(layout.NewBorderLayout(nil, nil, nil, timeSourceCheckButton), inputTimeSource, timeSourceCheckButton)
-	sourceContent := container.NewVBox(
-		sourceSelectRadio,
-		sourceFileContent,
-		setSourceMessage,
-		timeSourceContent,
-		setTimeSourceMessage,
-	)
+
+	gs := "gs://automediacheckpoint/"
+
 	outputContent := widget.NewMultiLineEntry()
 	outputContent.SetPlaceHolder("time line")
+	var texts []string
+	outputButton := widget.NewButton("MAKE INDEX", func() {
+		if setSourceStatus == 0 && setTimeSourceStatus == false {
+			outputContent.Text = "No Correct Source : MovieFile OR TimeFile !!"
+			outputContent.Refresh()
+		} else if setSourceStatus == 1 {
+			name := inputTimeSource.Selected[:strings.Index(inputTimeSource.Selected, ".txt")]
+			log.Printf("name : %s", name)
+			movie.MtoW(name)
+			sound.CutSoundFile(name, CheckPoint)
+			for i := 0; i < len(CheckPoint)-1; i++ {
+				TextCut := GCP.Captionasync(gs + name + "cut" + strconv.Itoa(i) + ".wav")
+				fmt.Println(TextCut)
+				texts = append(texts, TextCut)
+			}
+			log.Println(texts)
+			word.ReturnKeyWords(texts)
+		} else {
+			name := strings.Trim(inputTimeSource.Selected, ".txt")
+			movie.DlFromYT(inputFileSource.Text, name)
+			sound.CutSoundFile(name, CheckPoint)
+			for i := 0; i < len(CheckPoint)-1; i++ {
+				TextCut := GCP.Captionasync(gs + name + "cut" + strconv.Itoa(i) + ".wav")
+				fmt.Println(TextCut)
+				texts = append(texts, TextCut)
+			}
+			log.Println(texts)
+			word.ReturnKeyWords(texts)
+		}
+	})
 	copyButton := widget.NewButton("copy", func() {
 		clipboard.WriteAll(outputContent.Text)
 		log.Println(outputContent.Text)
 	})
+
+	sourceContent := container.NewVBox(
+		sourceFileContent,
+		setSourceMessage,
+		timeSourceContent,
+		setTimeSourceMessage,
+		outputButton,
+	)
 	outputtab := container.New(layout.NewBorderLayout(sourceContent, copyButton, nil, nil), sourceContent, outputContent, copyButton)
 
 	//##############################統合処理##############################
@@ -233,6 +304,24 @@ func main() {
 	myWindow.ShowAndRun()
 }
 
+func dirwalk(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			paths = append(paths, dirwalk(filepath.Join(dir, file.Name()))...)
+			continue
+		}
+		paths = append(paths, file.Name())
+	}
+
+	return paths
+}
+
 /*import (
 	"flag"
 	"fmt"
@@ -246,7 +335,7 @@ func main() {
 func main() {
 	path := "../lib/yuki_mono_VM00_VF00_0750.wav"
 	filename := strings.Trim(path[8:], ".wav")
-	gs := "gs://mystrage_19813/"
+	gs := "gs://automediacheckpoint/"
 	CheckPoint := []float64{0, 69, 138, 207, 276, 345}
 	//length := len(CheckPoint) - 1
 	//capacity := len(CheckPoint) - 1
